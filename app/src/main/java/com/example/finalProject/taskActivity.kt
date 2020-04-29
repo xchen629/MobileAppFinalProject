61aaf06f058b6689daedc5ba3dbc60981789f3fd
Daniel-Pobidel <dpobidel@yahoo.com>
2020-04-28 23:42:08 -0400

    task page changes
    -Added title to task page
    -new random tasked added to top of list upon button click
    -user can select from list of task categories and generate task of that specific type
    -changed title above app

--- app/src/main/java/com/example/finalProject/taskActivity.kt
+++ app/src/main/java/com/example/finalProject/taskActivity.kt
@@ -3,6 +3,7 @@ package com.example.finalProject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_task.*
 import retrofit2.Call
@@ -16,26 +17,25 @@ class taskActivity : AppCompatActivity() {
     private val BASE_URL = "https://www.boredapi.com/api/activity/"
     private val TAG = "MainActivity"
 
+    // Define an array to store a list of users
+    private val taskList = ArrayList<Task>()
+
+    // specify a viewAdapter for the dataset
+    val adapter = TasksAdapter(taskList)
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    val randomTaskAPI = retrofit.create(RandomTaskService::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
+    }
 
-        // Define an array to store a list of users
-        val taskList = ArrayList<Task>()
-
-        // specify a viewAdapter for the dataset
-        val adapter = TasksAdapter(taskList)
+    fun addRandTask(view:View){
         task_recycler_view.adapter = adapter
-
-        // use a linear layout manager
         task_recycler_view.layoutManager = LinearLayoutManager(this)
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val randomTaskAPI = retrofit.create(RandomTaskService::class.java)
 
         // Using enqueue method allows to make asynchronous call without blocking/freezing main thread
         randomTaskAPI.getTask().enqueue(object : Callback<Task>{
@@ -52,13 +52,41 @@ class taskActivity : AppCompatActivity() {
                     Log.w(TAG, "Valid response was not received")
                     return
                 }
-                
+
                 // Update the adapter with the new data
-                taskList.add(body)
+                taskList.add(0,body)
                 adapter.notifyDataSetChanged()
             }
-
         })
-
     }
+
+    fun addTypeTask(view:View){
+        task_recycler_view.adapter = adapter
+        task_recycler_view.layoutManager = LinearLayoutManager(this)
+        val category = categorySpinner.selectedItem.toString().toLowerCase()
+
+        // Using enqueue method allows to make asynchronous call without blocking/freezing main thread
+        randomTaskAPI.getTaskOfType(category).enqueue(object : Callback<Task>{
+            override fun onFailure(call: Call<Task>, t: Throwable) {
+                Log.d(TAG, "onFailure : $t")
+            }
+
+            override fun onResponse(call: Call<Task>, response: Response<Task>) {
+                Log.d(TAG, "onResponse: $response")
+                Log.d(TAG, "onResponse: ${response.body()?.activity}")
+                val body = response.body()
+
+                if (body == null){
+                    Log.w(TAG, "Valid response was not received")
+                    return
+                }
+
+                // Update the adapter with the new data
+                taskList.add(0,body)
+                adapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+
 }
