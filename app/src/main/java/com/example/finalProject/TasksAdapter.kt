8d2726452dbef3194ab1ac59415777f64e0ee176
Daniel-Pobidel <dpobidel@yahoo.com>
2020-04-30 12:19:18 -0400

    removing tasks from list
    -added onLongPressListener to task list
    -pops up alert dialog and user can just to save or remove
    -only remove functionality currently implemented

--- app/src/main/java/com/example/finalProject/TasksAdapter.kt
+++ app/src/main/java/com/example/finalProject/TasksAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.finalProject
 
+import android.app.AlertDialog
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import io.opencensus.metrics.LongGauge
 import kotlinx.android.synthetic.main.row_item.view.*
+import java.security.AccessController.getContext
 
 
 class TasksAdapter(private val tasks: ArrayList<Task>) : RecyclerView.Adapter<TasksAdapter.MyViewHolder>() {
@@ -39,10 +44,63 @@ class TasksAdapter(private val tasks: ArrayList<Task>) : RecyclerView.Adapter<Ta
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
-    inner class MyViewHolder (itemView: View): RecyclerView.ViewHolder (itemView){
+    inner class MyViewHolder (itemView: View): RecyclerView.ViewHolder (itemView) {
         // This class will represent a single row in our recyclerView list
         // This class also allows caching views and reuse them
         // Each MyViewHolder object keeps a reference to 3 view items in our row_item.xml file
         val activityName = itemView.tv_name
+
+        // Set onClickListener to show a toast message for the selected row item in the list
+        init {
+            /*itemView.setOnClickListener{
+                val selectedItem = adapterPosition
+                Toast.makeText(itemView.context, "You clicked on $selectedItem",
+                    Toast.LENGTH_SHORT).show()
+            }
+            */
+            // Set onLongClickListener to show a toast message and remove the selected row item from the list
+            itemView.setOnLongClickListener {
+                val selectedItem = adapterPosition //index of selected item
+                showDeleteOrSaveDialog(itemView,selectedItem)
+
+                return@setOnLongClickListener true
+            }
+        }
+
+
+        fun showDeleteOrSaveDialog(view: View, selectedItem:Int){
+            val selectedItemValue = tasks[selectedItem].activity
+            //val mDialogView = LayoutInflater.from(view.context).inflate(R.layout.customDialog, null)
+            // Create an alertdialog builder object,
+            // then set attributes that you want the dialog to have
+            val builder = AlertDialog.Builder(view.context)
+            builder.setTitle("You Selected: $selectedItemValue")
+            builder.setMessage("What would you like to do with this task?")
+            // Set an icon, optional
+            //builder.setIcon(android.R.drawable.ic_delete)
+
+            // Set the button actions, all of them are optional
+            builder.setPositiveButton("DELETE") { dialog, which ->
+                tasks.removeAt(selectedItem)
+                notifyItemRemoved(selectedItem)
+                Toast.makeText(
+                    itemView.context, "Deleting Task: $selectedItemValue",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            builder.setNegativeButton("SAVE") { dialog, which ->
+                //save here
+                //showSaveChoiceDialog()
+            }
+
+            builder.setNeutralButton("Cancel") { dialog, which ->
+            }
+
+            // create the dialog and show it
+            val dialog = builder.create()
+            dialog.show()
+        }
+
     }
 }
